4ee67509705e7fa375a87418002083cd52cdfe60
MartinAmante <martinjamante5@gmail.com>
2024-11-01 17:19:37 -0300

    arreglosv2

--- Assets/Scripts/Characters/Player/PlayerInput.cs
+++ Assets/Scripts/Characters/Player/PlayerInput.cs
@@ -2,50 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerInput : MonoBehaviour{
-    [SerializeField]public CharacterData player;
-
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField] public CharacterData player;
     public delegate void DirectionalInput(float xAxis, float yAxis);
-    public static DirectionalInput direction;
-
     public delegate void DashInput();
-    public static DashInput dash;
-
     public delegate void AttackInput();
-    public static AttackInput attack;
-
     public delegate void ProtectInput();
-    public static ProtectInput protect;
-
     public delegate void ProtectReleaseInput();
-    public static ProtectReleaseInput protectRelease;
-
     public delegate void InteractInput();
+    public delegate void InteractChestInput();
+    public static DirectionalInput direction;
+    public static DashInput dash;
+    public static AttackInput attack;
+    public static ProtectInput protect;
+    public static ProtectReleaseInput protectRelease;
     public static InteractInput interact;
-    private float xAxis, yAxis;
-    private bool directionPressed, directionReleased, dashPressed, attackPressed, protectPressed, protectReleased;
+    public static InteractChestInput interactChest;
 
-    void Update(){
-        xAxis = Input.GetAxis("Horizontal");
-        yAxis = Input.GetAxis("Vertical");
-        if(player.IsAlive){
-            directionPressed = xAxis != 0 || yAxis != 0;
-            dashPressed = Input.GetButton("Dash");
-            attackPressed = Input.GetButton("Attack");
-            protectPressed = Input.GetButton("Protect");
-        }else{
-            directionPressed = false;
-            dashPressed = false;
-            attackPressed = false;
-            protectPressed = false;
+    void Update()
+    {
+        if (player.IsAlive)
+        {
+            direction(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            if (Input.GetButton("Dash")) dash();
+            if (Input.GetButton("Attack")) attack();
+            if (Input.GetButton("Protect")) { protect(); } else { protectRelease(); }
+            if (Input.GetButton("Interact")) interact();
+            if (Input.GetButton("InteractChest"))interactChest(); 
         }
-
-        if(directionPressed) {direction(xAxis, yAxis); directionReleased = false;}
-        if(!directionPressed && !directionReleased) {direction(0, 0); directionReleased = true;}
-        if(dashPressed) dash();
-        if(attackPressed) attack();
-        if(protectPressed) {protect(); protectReleased = false;}
-        if(!protectPressed && !protectReleased) {protectRelease();  protectReleased = true;}
-        //if(Input.GetButton("Interact")) interact();
     }
 }
