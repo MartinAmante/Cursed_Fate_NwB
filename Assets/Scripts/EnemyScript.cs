edb0f6a481f61fb47e326bcf4e2e29eec6e61e8d
Ezemr12 <110836240+Ezemr12@users.noreply.github.com>
2024-11-10 22:11:17 -0300

    Varios Cambios.
    Se agregaron los mapas 1,2 y 3. También se agrego el script para que los enemigos sigan al player, se agregaron los generadores de enemigos a los mapas, por ultimo se hizo que el menú pueda cambiar entre escenas.

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public GameObject player; // Referencia al jugador
+    public float speed = 3f;  // Velocidad de movimiento del enemigo
+
+    private Transform playerTransform;
+
+    void Start()
+    {
+        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+        if (playerObject != null)
+        {
+            playerTransform = playerObject.transform;
+        }
+    }
+
+    void Update()
+    {
+        // Mueve al enemigo hacia el jugador
+        if (playerTransform != null)
+        {
+            Vector2 direction = (playerTransform.position - transform.position).normalized;
+            transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, speed * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Verifica si el enemigo toca al jugador
+        if (other.CompareTag("Player"))
+        {
+            GameObject.FindGameObjectWithTag("Portal").GetComponent<Portal>().EnemigoEliminado();
+            Destroy(gameObject); // Destruye al enemigo
+        }
+    }
+}
